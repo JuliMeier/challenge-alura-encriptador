fea9a84431c4b10a75faccff6aac26e6015fa858
Julian Meier <julianrg320@gmail.com>
2024-08-15 09:10:25 -0300

    inicializando el js

---
+++ script.js
@@ -0,0 +1,14 @@
+const llaves = [
+    ["a", "ai"],
+    ["e", "enter"],
+    ["i", "imes"],
+    ["o", "ober"],
+    ["u", "ufat"],
+];
+
+function encriptarMensaje(mensaje){
+    let mensajeEncriptado = "";
+    for (let i = 0; i < mensaje.length; i++){
+        let letra = mensaje[i];
+    }
+}
